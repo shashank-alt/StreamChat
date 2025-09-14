8db2c4d2a6613083c67d5e2d228b717aca436cee
Shashank <shashank29082004@gmail.com>
2025-09-14 20:34:39 +0530

    updating backend url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5001/api",
+    baseURL: "https://streamchat-backend-6ltp.onrender.com/api",
     withCredentials: true
 });
 
